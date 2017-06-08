7feef69a71718626186c2b5d90b5bd26f5cfdf8d
naufaldwi <naufaldwi08>
2017-06-08 19:08:30 +0700

    aaa

---
+++ src/ini/bank/ya/Bendahara.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ini.bank.ya;
+
+/**
+ *
+ * @author Zeus Maxwell
+ */
+public class Bendahara {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
